98b4120b3649a66c583f768ab56f94df19f6735d
Study_with_Munir <studywithmunir18@gmail.com>
2024-01-02 19:05:10 +0500

    Add project files.

---
+++ Munir015_Green_spaces_Finder/Server/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Munir015_Green_spaces_Finder.Shared;
+using System.Security.Claims;
+
+namespace Munir015_Green_spaces_Finder.Server.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<GreenSpace> GreenSpaces { get; set; }
+        public DbSet<SearchHistory> SearchHistories { get; set; }
+        public DbSet<UserFavorite> UserFavorites { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+    }
+}
---
+++ Munir015_Green_spaces_Finder/Server/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Munir015_Green_spaces_Finder.Server.Data;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+//Database Services
+var connectionString = "Data Source=DESKTOP-QLCANRF\\SQLEXPRESS;Initial Catalog=AsgGreenspace;integrated security=true;TrustServerCertificate=True";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
